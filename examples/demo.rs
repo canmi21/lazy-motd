f7ec8feb99235eec47743d2aed067ffb7b673a01
Canmi <9997200@qq.com>
2025-09-03 00:45:22 +0800

    feat: Add new examples for lazy_motd macro with bin and build arguments

--- examples/demo.rs
+++ examples/demo.rs
@@ -2,6 +2,8 @@
 
 fn main() {
     lazy_motd::lazy_motd!();
-    lazy_motd::lazy_motd!("(Nightly)");
-    lazy_motd::lazy_motd!("None");
+    lazy_motd::lazy_motd!(build = "(Nightly)");
+    lazy_motd::lazy_motd!(build = "None");
+    lazy_motd::lazy_motd!(bin = "my_custom_bin");
+    lazy_motd::lazy_motd!(bin = "my_bin", build = "Nightly");
 }
