36511f73167aae50479433aab54938301fb7cb72
Canmi <9997200@qq.com>
2025-09-03 00:54:15 +0800

    refactor: Update example usages of lazy_motd macro

--- examples/demo.rs
+++ examples/demo.rs
@@ -2,8 +2,9 @@
 
 fn main() {
     lazy_motd::lazy_motd!();
-    lazy_motd::lazy_motd!(build = "(Nightly)");
+    lazy_motd::lazy_motd!(build = "Nightly");
     lazy_motd::lazy_motd!(build = "None");
-    lazy_motd::lazy_motd!(bin = "my_custom_bin");
-    lazy_motd::lazy_motd!(bin = "my_bin", build = "Nightly");
+    lazy_motd::lazy_motd!(bin = "Custom");
+    lazy_motd::lazy_motd!(bin = "None");
+    lazy_motd::lazy_motd!(bin = "Example", build = "Nightly");
 }
