37e43efb10a1679e4af2c8d6d95b7eb0d82753e6
Canmi <9997200@qq.com>
2025-09-03 08:53:02 +0800

    feat: enhance lazy_motd macro with optional timestamp

--- src/lib.rs
+++ src/lib.rs
@@ -4,19 +4,22 @@ pub mod pkg;
 
 #[macro_export]
 macro_rules! lazy_motd {
-    (bin=$bin_name:literal, build=$build_info:literal) => {{
+    (bin=$bin_name:literal, build=$build_info:literal, timestamp=$ts:literal) => {{
         let pkg_name = $crate::resolve_pkg_name!($bin_name);
-        if let Err(e) = $crate::pkg::print_motd(pkg_name, $build_info) {
+        if let Err(e) = $crate::pkg::print_motd(pkg_name, $build_info, $ts) {
             eprintln!("Failed to print motd: {}", e);
         }
     }};
+    (bin=$bin_name:literal, build=$build_info:literal) => {
+        $crate::lazy_motd!(bin = $bin_name, build = $build_info, timestamp = "");
+    };
     () => {
-        $crate::lazy_motd!(bin = "", build = "Preview");
+        $crate::lazy_motd!(bin = "", build = "Preview", timestamp = "");
     };
     (build=$build_info:literal) => {
-        $crate::lazy_motd!(bin = "", build = $build_info);
+        $crate::lazy_motd!(bin = "", build = $build_info, timestamp = "");
     };
     (bin=$bin_name:literal) => {
-        $crate::lazy_motd!(bin = $bin_name, build = "Preview");
+        $crate::lazy_motd!(bin = $bin_name, build = "Preview", timestamp = "");
     };
 }
