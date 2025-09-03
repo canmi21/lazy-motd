f862ec17d6e944be368ba434c88e361a535cbe5e
Canmi <9997200@qq.com>
2025-09-03 08:45:45 +0800

    refactor: move print_motd to pkg module and use resolve_pkg_name macro

--- src/lib.rs
+++ src/lib.rs
@@ -1,40 +1,12 @@
 /* src/lib.rs */
 
+pub mod pkg;
+
 #[macro_export]
 macro_rules! lazy_motd {
     (bin=$bin_name:literal, build=$build_info:literal) => {{
-        use ::std::io::Write;
-        use ::termcolor::WriteColor;
-        let print_motd = || -> ::std::io::Result<()> {
-            let mut stdout = ::termcolor::StandardStream::stdout(::termcolor::ColorChoice::Auto);
-            let pkg_name = if $bin_name == "None" {
-                env!("CARGO_BIN_NAME")
-            } else if $bin_name != "" {
-                $bin_name
-            } else {
-                env!("CARGO_PKG_NAME")
-            };
-            let pkg_version = env!("CARGO_PKG_VERSION");
-            let timestamp = ::chrono::Local::now().format("%Y-%m-%d %H:%M:%S");
-            writeln!(&mut stdout)?;
-            stdout.set_color(
-                ::termcolor::ColorSpec::new().set_fg(Some(::termcolor::Color::Magenta)),
-            )?;
-            write!(&mut stdout, "  ▲ {} {}", pkg_name, pkg_version)?;
-            stdout.reset()?;
-            if $build_info != "" && $build_info != "None" {
-                if $build_info.starts_with("(") {
-                    write!(&mut stdout, " {}", $build_info)?;
-                } else {
-                    write!(&mut stdout, " ({})", $build_info)?;
-                }
-            }
-            writeln!(&mut stdout)?;
-            writeln!(&mut stdout, "  - Timestamp: {}", timestamp)?;
-            writeln!(&mut stdout)?;
-            Ok(())
-        };
-        if let Err(e) = print_motd() {
+        let pkg_name = $crate::resolve_pkg_name!($bin_name);
+        if let Err(e) = $crate::pkg::print_motd(pkg_name, $build_info) {
             eprintln!("Failed to print motd: {}", e);
         }
     }};
